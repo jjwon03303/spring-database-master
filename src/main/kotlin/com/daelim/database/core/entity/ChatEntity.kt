4bf51b189a1d3f44e32ad11049030add7005b7d9
jjwon03303 <senseme538@gmail.com>
2024-05-08 16:38:47 +0900

    small test를 2개 만듬 1. 70자 이내 확인 2. a라는 글자가 있는지 확인

--- src/main/kotlin/com/daelim/database/core/entity/ChatEntity.kt
+++ src/main/kotlin/com/daelim/database/core/entity/ChatEntity.kt
@@ -7,7 +7,7 @@ import java.util.Date
 
 @Entity
 @Table(name = "chats")
-data class Chat(
+data class ChatEntity(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var time: String? = null,
--- src/test/kotlin/com/daelim/database/small/ChatSpec.kt
+++ src/test/kotlin/com/daelim/database/small/ChatSpec.kt
@@ -1,12 +1,59 @@
 package com.daelim.database.small
 
+import com.daelim.database.core.entity.ChatEntity
 import org.springframework.boot.test.context.SpringBootTest
 import net.datafaker.Faker
+import org.apache.juli.logging.Log
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
 
 
 @SpringBootTest
 class ChatSpec {
     private val faker = Faker()
 
+    private lateinit var randomChat : ChatEntity
 
+    @BeforeEach //시작하기 전에 실행될 메서드를 저장해둠.
+    // username과 password가 테스트마다 새롭게 들어감
+    fun setup() {
+        val username = faker.internet().username()
+        val chat = faker.lorem().sentence(10, 5)
+        randomChat = ChatEntity(
+            time = LocalDateTime.now().toString(),
+            username = username,
+            chat = chat,
+        )
+    }
+
+    //테스트로 지정되어있다면 왼쪽의 재생버튼을 누르면 테스트코드로 실행된다
+    @Test
+    @DisplayName("chat이 70자 이내인지 확인")
+    fun `"chat이 70자 이내인지 확인"`() {
+
+        // When
+        val rdChat = randomChat.chat
+
+        // Then
+        Assertions.assertTrue(rdChat.length <= 70)
+        //rdChat의 길이가 70자 이하라면
+    }
+
+    @Test
+    @DisplayName("a라는 단어가 있는지 확인하고, 있다면 a를 b로 바꾸기")
+    fun `"a라는 단어가 있는지 확인하고, 있다면 a를 b로 바꾸기"`() {
+
+        // When
+        val rdChat = randomChat.chat
+
+        // Then
+        rdChat.replace('a','b')
+        //chat 내부의 a를 전부 b로 바꾸고
+        Assertions.assertFalse(rdChat.contains('a'))
+        //a를 가지고 있는게 False이여야함
+
+    }
 }
